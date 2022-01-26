2be2643406131a4b2f4f05edf80fccfd177b5c41
lingyunyi <43637894+lingyunyi@users.noreply.github.com>
2022-01-26 11:25:30 +0800

    Add files via upload

--- 210813 - Dj_infoMana/urls_viewShow/dj_dataToolsVs.py
+++ 210813 - Dj_infoMana/urls_viewShow/dj_dataToolsVs.py
@@ -7,64 +7,27 @@ from unit_Tools.sqlData_unitTools import sqlData_unitTools
 
 sqlData_unitToolsC = sqlData_unitTools()
 
-def Info_exportExcel(request):
+def API_db_backups_save(request):
     '''
-        主题展示 视图函数
     :param request:
     :return:
     '''
-    if request.META.get('HTTP_X_FORWARDED_FOR'):
-        ip = request.META['HTTP_X_FORWARDED_FOR']
-    else:
-        ip = request.META['REMOTE_ADDR']
-
+    ret = {"status": "fales", "mesg": "Null"}
     try:
-        uuid4_str = request.COOKIES.get("username_id")
-        user_nameQ = str(request.session.get(uuid4_str))
-        user_name = hashlib.md5(str(user_nameQ).encode("utf-8")).hexdigest()
-        if uuid4_str == None:
-            print("-" * 100)
-            return redirect('/')
+        DB_USER = 'root'
+        DB_PASSWORD = 'root'
+        DB_NAME = 'equipment_manager'
+        TODAY = time.strftime('%Y-%m-%d-%H-%M-%S')  #
+        BACK_DIR= r'E:\PythonProject\equipment_manager_sys\static\backups_dir/'
+        TODAY_DIR = BACK_DIR + TODAY
+        # 如果目录不存在，新建目录
+        # 执行mysql命令，导出数据库到新建的文件
+        mysql_dump_dir = r'D:\xampp\MySQL\bin\mysqldump.exe'
+        sqlcmd = mysql_dump_dir + " --single-transaction -u" + DB_USER + " -p" + DB_PASSWORD + " " + DB_NAME + " > " + TODAY_DIR  + ".sql"
+        print(sqlcmd)
+        res = os.popen(sqlcmd)
+        print(res)
+        ret["status"] = "true"
     except BaseException as e:
-        print("-" * 50, e)
-        return redirect('/')
-
-    if request.method == "GET":
-        print("-" * 15, "ViewFunc Info_exportExcel - Get", "-" * 15)
-        sql = '''select * from infomana where accountName = %s order by id desc limit 1000'''
-        infoMalist = sqlData_unitToolsC.search(sql,[user_name],show=False)
-        excelInsertValue = []
-        if infoMalist:
-            for i in infoMalist:
-                excelInsertValueCell = []
-                insert_Dict = json.loads(i[1])
-                excelInsertValueCell.append(insert_Dict["title"])
-                for i in insert_Dict.get("infoList",None):
-                    excelInsertValueCell.append(i.get("content",None))
-                excelInsertValue.append(excelInsertValueCell)
-            path = "static/Excel/" + time.strftime("%m-%d-%H-%M-%S") +".xls"
-            write_excel_xls(path,"one",excelInsertValue)
-
-            file = open(path, 'rb')
-            response = HttpResponse(file)
-            response['Content-Type'] = 'application/octet-stream'
-            response['Content-Disposition'] = 'attachment;filename="%s"'%(path.split("/")[-1])
-            return response
-        else:
-            print("-" * 15, "数据库无值，不可导出数据。", "-" * 15)
-            return redirect('/activity_show')
-    elif request.method == "POST":
-            pass
-
-
-
-
-def write_excel_xls(path, sheet_name, value):
-    index = len(value)  # 获取需要写入数据的行数
-    workbook = xlwt.Workbook()  # 新建一个工作簿
-    sheet = workbook.add_sheet(sheet_name)  # 在工作簿中新建一个表格
-    for i in range(0, index):
-        for j in range(0, len(value[i])):
-            sheet.write(j, i, value[i][j])  # 像表格中写入数据（对应的行和列）
-    workbook.save(path)  # 保存工作簿
-    print("-" * 15, "ViewFunc write_excel_xls - Get", "-" * 15)
+        ret["mesg"] = e
+    return HttpResponse(json.dumps(ret))
